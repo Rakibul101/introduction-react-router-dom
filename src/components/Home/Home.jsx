af69290af28823bd6b2149722bb04480358b1ab3
Rakibul101 <rakibulislam.dev07@gmail.com>
2024-07-15 04:58:47 +0600

    react project done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
-
+import { Audio } from 'react-loader-spinner';
 const Home = () => {
+    const navigation = useNavigation();
+    console.log(navigation);
     return (
         <div>
             <h2>This is the home components</h2>
             <Header></Header>
-            <Outlet></Outlet>
+            {
+                navigation.state === "loading"? <p><Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="green"
+                ariaLabel="loading"
+                wrapperStyle
+                wrapperClass
+              /></p>: 
+                <Outlet></Outlet>
+            }
+            
         </div>
     );
 };
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,17 +1,25 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-const Post = ({post}) => {
-    const {id, title, body, userId} = post;
+import { Link, useNavigate } from 'react-router-dom';
+
+
+
+const Post = ({ post }) => {
+    const { id, title, body, userId } = post;
+    const navigate = useNavigate();
     const postStyle = {
         border: "1px solid green",
         padding: "10px",
         borderRadius: "15px"
+    }
+    const handleShowDetail = () => {
+        navigate(`/post/${id}`)
     }
     return (
         <div style={postStyle}>
             <h2>Title: {title}</h2>
             <p>Description: {body}</p>
             <Link to={`/post/${id}`}>Show Details</Link>
+            <button onClick={handleShowDetail}>Click to see detail</button>
         </div>
     );
 };
